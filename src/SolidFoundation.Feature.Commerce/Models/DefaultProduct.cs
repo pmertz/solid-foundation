e2528dc0bef0ba8cf6582fd2f6440c2eec28cee4
Palle Mertz <pmertz@gmail.com>
2023-02-25 13:31:27 +0100

    Added commerce product to make the website start

---
+++ src/SolidFoundation.Feature.Commerce/Models/DefaultProduct.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.Catalog.DataAnnotations;
+
+namespace SolidFoundation.Feature.Commerce.Models;
+
+[CatalogContentType(
+    DisplayName = "Default Product", 
+    GUID = "{515B80D9-BB72-495C-89B6-916FA9C2F695}")]
+public class DefaultProduct : ProductContent
+{
+}
